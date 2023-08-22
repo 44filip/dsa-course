9a313ea4b8acf1aa5e1ba7d146feb77881f96732
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-08-22 20:40:28 +0200

    Initial commit
    Importing Treehouse course work

---
+++ recursive_binary_search.py
@@ -0,0 +1,21 @@
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+def recursive_binary_search(_list, target):
+    if len(_list) == 0:
+        return False
+    else:
+        midpoint = (len(_list))//2
+        
+        if _list[midpoint] == target:
+            return True
+        else:
+            if _list[midpoint] < target:
+                return recursive_binary_search(_list[midpoint+1:], target)
+            else:
+                return recursive_binary_search(_list[:midpoint], target)
+            
+def verify(result):
+    print("Target found:", result)
+
+result = recursive_binary_search(numbers, 3)
+verify(result)
