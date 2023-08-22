6814a382bd420e04f0dbc04e2fd7b737264f4fe1
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-08-22 23:22:53 +0200

    Finalizing & comments
    Completing the fully functional quicksort for numbers and commenting useful explanations.

--- quicksort.py
+++ quicksort.py
@@ -5,3 +5,27 @@ import sys
 from load import load_numbers
 
 numbers = load_numbers(sys.argv[1])
+
+def quicksort(_list):
+    if len(_list) <= 1:                         # Set up a base case which stops the recursion
+        return _list
+    
+    less_than_pivot = []                        # Initialize lesser list as an empty list
+    greater_than_pivot = []                     # Initialize greater list as an empty list
+    pivot = _list[0]                            # Set pivot as the element at index 0 (first element)
+    for element in _list[1:]:                   # Iterate every element excluding the pivot
+        if element <= pivot:                    # If the element is lesser, add it to the less_than list
+            less_than_pivot.append(element)
+        else:
+            greater_than_pivot.append(element)  # If the element is greater, add it to the greater_than list
+            
+            """
+            Quicksorts the list and all its sublists in a stack until it reaches the base case
+            When the base case is met, the stack unveils, and the values are sorted
+            The end values are then added before and after the pivot respectively
+            """
+    return quicksort(less_than_pivot) + [pivot] + quicksort(greater_than_pivot)
+            
+    
+sorted_numbers = quicksort(numbers)
+print(sorted_numbers)
