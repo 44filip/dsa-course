321712cfd1b71319dc9e3614b55d9f6627a274dc
S-Filip <100999946+S-Filip@users.noreply.github.com>
2023-08-24 22:39:22 +0200

    Max-heapify implementation
    Added and tested max-heapify and build_max_heap functions with descriptive comments.

--- heapq_module.py
+++ heapq_module.py
@@ -7,11 +7,6 @@ Maxheap - children of each node have to be equal or lesser than the node
 
 Heaps can be used to implement a priority queue
 A priority queue pops the highest priority element (lowest number)
-
-To find the children of a specific node in a list visual of a heap:
-i = node index
-2*i+1 = first child
-2*1+2 = second child
 """
 
 def flipList(_list):
@@ -49,3 +44,53 @@ l3 = heapq.merge(l1,l2)
 # The result is a generator, hence we must use list
 # Generators are functions that give one value at a time
 print("Heapmerged data (l1 & l2): \n%s\n" % list(l3))
+
+
+# To find the specific nodes from a heap in list form follow the code below
+def left_child(i):
+    return 2*i
+
+def right_child(i):
+    return 2*i + 1
+
+def parent(i):
+    return i//2
+# End of code snippet for locating nodes
+
+
+def max_heapify(a, heap_size, i):
+    """
+    Runs in O(log n)
+    """
+    l = left_child(i)
+    r = right_child(i)
+
+    largest = i 
+
+    # Compare left child with target node
+    if l < heap_size and a[l] > a[i]:
+    # If the left child is greater, assign it the pointer
+        largest = l
+    
+    # Compare right child with target node (or left child (step above))
+    if r < heap_size and a[r] > a[largest]:
+    # If the right child is greater, assign it the pointer
+        largest = r 
+    
+    # If the pointer isn't at the target node
+    if largest != i:
+        # Swap the elements to fix it
+        a[i], a[largest] = a[largest], a[i]
+        # Then recursively max_heapify the children
+        max_heapify(a, heap_size, largest)
+
+
+def build_max_heap(_list):
+    heap_size = len(_list)
+
+    # This part confuses me, will try to understand later
+    for i in range(heap_size//2, -1, -1):
+        max_heapify(_list, heap_size, i)
+        
+build_max_heap(data)
+print("Maxheapified data: \n%s\n" % data)
